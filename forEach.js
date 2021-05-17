ee391fa638f4925a8b58360e5ef92436331aab94
Hrachuhi Sepkhanyan <hrachuhisepkhanyan@gmail.com>
2021-05-18 01:40:27 +0400

    method of foreach

---
+++ forEach.js
@@ -0,0 +1,13 @@
+const array = ["a", "b", "c", "d"]
+array.forEach(function(val) {
+    console.log(val);
+})
+
+// function forEach(arr, fun) {
+//     for (let index = 0; index < array.length;) {
+//         let elem = arr[index];
+
+//         fun(elem);
+//         index++
+//     }
+// };
